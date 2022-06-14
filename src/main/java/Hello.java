dc71020c9a872efe0d6e42f5e7eaff38c7dda3c2
Ali Khan <alikhan@Alis-MacBook-Air.local>
2022-06-14 12:30:38 +0500

    commit after poll scm

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -10,7 +10,7 @@ public class Hello extends HttpServlet {
 
    public void init() throws ServletException {
       // Do required initialization
-      message = "Hello World this .java file";
+      message = "Hello World this .java file after Poll SCM";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
